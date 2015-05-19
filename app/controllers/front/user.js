079b567f06c76df239b8c28d379fabe81b4ef893
charmyin <976582265@qq.com>
2015-05-20 00:08:55 +0800

    Developing user and brand manage

--- app/controllers/front/user.js
+++ app/controllers/front/user.js
@@ -27,6 +27,7 @@ exports.searchList = function(req, res) {
 
 exports.add = function (req, res) {
     delete req.body._id;
+    req.body.password="111111";
     var commonModel = new CommonModel(req.body);
     commonModel.save(function(err) {
         utils.setSaveResponse(err, res, commonModel);
